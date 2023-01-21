156a9a9845cd34e8f0ed28caad7180929c63f0c2
Alejandro Bravo <92216471+ciberHam@users.noreply.github.com>
2023-01-21 12:28:36 -0300

    v1.8 cv T final
    cambios varios de diseño, mejora del menu, aenxo iconos redes

--- funciones.js
+++ funciones.js
@@ -1,10 +1,32 @@
-﻿
+﻿$(document).ready(function(){
+	var mover_a = $(".desplazar");
+	
+	mover_a.click(function(evento){
+		evento.preventDefault();
+		$("body,html").animate(
+		{
+		scrollTop:$(this.hash).offset().top,
+		},1500
+		);
+	})
+})
 let posicionMenu=true;
 const claseContenido = document.getElementsByClassName("contContenido");
 let datos={};
-function callback(){
-	const archivoJasonVariable = JSON.parse(datos.json);
-	return archivoJasonVariable;
+async function callback(){
+	const response = await fetch('https://ciberham.github.io/repoDeArchivosJson/datos.json')
+	const json = await response.json();
+	console.log(json["nombreCompleto"])
+	document.getElementById("fullname").innerHTML = json.nombreCompleto;
+	document.getElementById("contenido").innerHTML = json.textoIntro;
+	document.getElementById("email").innerHTML = json.email;
+	document.getElementById("fNacimiento").innerHTML = json.fechaNacimiento;
+	document.getElementById("ciudad").innerHTML = json.ciudad;
+	document.getElementById("pais").innerHTML = json.paisOrigen;
+	document.getElementById("fullname").innerHTML = json.nombreCompleto;
+	document.getElementById("fullname").innerHTML = json.nombreCompleto;
+	document.getElementById("fullname").innerHTML = json.nombreCompleto;
+	document.getElementById("fullname").innerHTML = json.nombreCompleto;
 }
 
 
@@ -15,7 +37,7 @@ callback()
 		document.getElementById("ocultarMenu").style.display = "none";
 		document.getElementById("cambiaMenu").style.width="15%"
 			if(screen.width < 900){
-				document.getElementById("cambiaMenu").style.width="97%"
+				document.getElementById("cambiaMenu").style.width="100%"
 			}
 		document.getElementById("cambiaMenu").style.height="8%"
 		posicionMenu=true;
@@ -23,7 +45,7 @@ callback()
 		document.getElementById("ocultarMenu").style.display = "flex";
 	    document.getElementById("cambiaMenu").style.width="95.5%";
 			if(screen.width < 900){
-				document.getElementById("cambiaMenu").style.width="97%"
+				document.getElementById("cambiaMenu").style.width="100%"
 			}
 		document.getElementById("cambiaMenu").style.height="8%"
 		posicionMenu=false;
